146be84d28955ea79fa03258a726422c6aa1c094
Niels Holst <niels.holst@agrsci.dk>
2010-08-11 10:44:16 +0200

    Finish PullVariabvle refactoring
    Add forgotten PullVariableBase class

---
+++ src/library/usbase/pull_variable(0).h
@@ -0,0 +1,35 @@
+/* Copyright (C) 2009-2010 by Niels Holst [niels.holst@agrsci.dk] and co-authors.
+** Copyrights reserved.
+** Released under the terms of the GNU General Public License version 3.0 or later.
+** See www.gnu.org/copyleft/gpl.html.
+*/
+#ifndef UNISIM_PULL_VARIABLE_H
+#define UNISIM_PULL_VARIABLE_H
+
+#include <QObject>
+#include "identifier.h"
+
+namespace UniSim{
+
+class PullVariable : public QObject
+{
+    Q_OBJECT
+
+public:
+    PullVariable(Identifier name, const double *value, QObject *parent, QString desc);
+    double value() const;
+    const double* valuePtr() const;
+    Identifier id() const;
+    QString description() const;
+
+private:
+    Identifier _id;
+    const double *_valuePtr;
+    QString desc;
+};
+
+
+} //namespace
+
+#endif
+
---
+++ src/library/usbase/pull_variable_base.cpp
@@ -0,0 +1,29 @@
+/* Copyright (C) 2009-2010 by Niels Holst [niels.holst@agrsci.dk] and co-authors.
+** Copyrights reserved.
+** Released under the terms of the GNU General Public License version 3.0 or later.
+** See www.gnu.org/copyleft/gpl.html.
+*/
+
+#include "pull_variable_base.h"
+
+namespace UniSim{
+
+
+PullVariableBase::PullVariableBase(Identifier id, QObject *parent, QString description)
+    : QObject(parent), _id(id), _description(description)
+{
+    setObjectName(id.key());
+}
+
+Identifier PullVariableBase::id() const {
+    return _id;
+}
+
+QString PullVariableBase::description() const {
+    return _description;
+}
+
+
+} //namespace
+
+
